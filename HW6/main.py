5cdcc62487808203249f17cad12b35cdc3afbcea
HP <102493715+HPHYP@users.noreply.github.com>
2025-02-19 19:15:19 +0000

    We've added to main.py a bunch of examples on how to code in python

---
+++ HW6/main.py
@@ -0,0 +1,7 @@
+#Ani Toews, Homework 6, 2/19/2025
+
+input_1 = int(input("give me a number: ")) # asks user for first number
+input_2 = int(input("Give me another number: "))
+result = input_1+input_2
+
+print(f'The numbers added is {result}')
