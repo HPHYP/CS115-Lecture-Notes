347eaef494ac12af8cfdaf9000b7d06a9847f952
HP <102493715+HPHYP@users.noreply.github.com>
2025-02-24 18:34:12 +0000

    This is a test

--- HW6/main.py
+++ HW6/main.py
@@ -1,7 +1,7 @@
 #Ani Toews, Homework 6, 2/19/2025
 
-input_1 = int(input("give me a number: ")) # asks user for first number
-input_2 = int(input("Give me another number: "))
-result = input_1+input_2
+input_1 = int(input("give me a number: ")) # Turns string into int and asks user for first number
+input_2 = int(input("Give me another number: ")) # turns string into int and asks user for second number
+result = input_1+input_2 #stores both numbers being added into a variable
 
-print(f'The numbers added is {result}')
+print(f'The numbers added is {result}') #Displays the results of the variable alongside some text
